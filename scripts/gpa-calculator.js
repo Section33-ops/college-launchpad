f52c78bfdb13a66cbbf120b05fbeffcac1bb4c81
Saeed Wumbei Mamani <228374580+Section33-ops@users.noreply.github.com>
2026-06-26 11:53:45 -0400

    feat: create function to calculate gpa then show it to the user with dom

--- scripts/gpa-calculator.js
+++ scripts/gpa-calculator.js
@@ -2,9 +2,18 @@ const courseNameInput = document.getElementById('course-name');
 const gradeInput = document.getElementById('grade');
 const creditHoursInput = document.getElementById('credit-hours');
 const addCourseButton = document.getElementById('add-course-button');
+const calculateButton = document.getElementById('js-calculate-button');
 const courseRows = document.getElementById('course-rows');
 
 const courses = [];
+
+const gradeScale = {
+  A: 4.0,
+  B: 3.0,
+  C: 2.0,
+  D: 1.0,
+  F: 0.0,
+};
 
 function addCourseToTable(newCourse) {
   courseRows.insertAdjacentHTML(
@@ -25,6 +34,34 @@ function clearInputFields() {
   creditHoursInput.value = '';
 }
 
+function calculateGPA(courses) {
+  const coursesWithGradeValue = [];
+  let gradeLetter = '';
+  let totalPoints = 0;
+  let totalCredit = 0;
+  let gpa = 0;
+
+  courses.forEach((course) => {
+    gradeLetter = course.grade;
+    coursesWithGradeValue.push({ ...course, grade: gradeScale[gradeLetter] });
+  });
+
+  coursesWithGradeValue.forEach((course) => {
+    totalPoints += course.grade * course.creditHours;
+    totalCredit += Number(course.creditHours);
+  });
+
+  gpa = totalPoints / totalCredit;
+
+  console.log(coursesWithGradeValue);
+  console.log(totalPoints);
+  console.log(totalCredit);
+  console.log(gpa);
+
+  document.getElementById('gpa-result').innerHTML = `Your GPA is ${gpa}`;
+  return `Your GPA is ${gpa}`;
+}
+
 addCourseButton.addEventListener('click', (event) => {
   event.preventDefault();
 
@@ -41,4 +78,10 @@ addCourseButton.addEventListener('click', (event) => {
   clearInputFields();
 
   console.log(courses);
+});
+
+calculateButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  calculateGPA(courses);
 });
