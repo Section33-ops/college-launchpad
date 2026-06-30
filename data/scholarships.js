cdf9012dec149f07d7369458d381e06f5f41e775
Saeed Wumbei Mamani <228374580+Section33-ops@users.noreply.github.com>
2026-06-30 10:17:12 -0400

    feat: setup college finder and scholarships pages and also data

---
+++ data/scholarships.js
@@ -0,0 +1,47 @@
+export const scholarships = [
+  {
+    id: 1,
+    name: 'The Gates Scholarship',
+    description:
+      'A highly selective, last-dollar scholarship for outstanding, minority high school seniors from low-income households.',
+    minGPA: 3.3,
+    preferredMajors: ['All'],
+    amount: 'Full Cost of Attendance',
+  },
+  {
+    id: 2,
+    name: 'Coca-Cola Scholars Program',
+    description:
+      'An achievement-based scholarship awarded to graduating high school seniors recognized for their capacity to lead and serve.',
+    minGPA: 3.0,
+    preferredMajors: ['All'],
+    amount: '$20,000',
+  },
+  {
+    id: 3,
+    name: 'Jackie Robinson Foundation Scholarship',
+    description:
+      'Provides generous four-year financial grants accompanied by comprehensive mentoring and leadership development services.',
+    minGPA: 3.0,
+    preferredMajors: ['All'],
+    amount: 'Up to $35,000 over 4 years',
+  },
+  {
+    id: 4,
+    name: 'Amazon Future Engineer Scholarship',
+    description:
+      'Awarded to high school seniors planning to study computer science or engineering, which includes a paid summer internship at Amazon.',
+    minGPA: 3.0,
+    preferredMajors: ['STEM'],
+    amount: '$40,000 ($10,000/year)',
+  },
+  {
+    id: 5,
+    name: 'Lockheed Martin STEM Scholarship',
+    description:
+      'Open to high school seniors and undergraduate students who plan to major in specified STEM fields.',
+    minGPA: 2.5,
+    preferredMajors: ['STEM'],
+    amount: '$10,000 / year',
+  },
+];
