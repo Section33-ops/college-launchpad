89397d77e7649b6feaf7b4cf2c01721cbfbcb3b5
Saeed Wumbei Mamani <228374580+Section33-ops@users.noreply.github.com>
2026-07-07 20:46:24 -0400

    feat: add event listener to search button to get college list based on user's city and state entered

--- scripts/collegeFinder.js
+++ scripts/collegeFinder.js
@@ -1,6 +1,9 @@
 import { colleges } from '../data/colleges.js';
+import { fetchCollege } from './collegeApi.js';
 
 const tableBody = document.getElementById('js-college-rows');
+
+const searchButton = document.querySelector('#js-search-button');
 
 function addCollegesToTable(collegeList) {
   let collegeHtml = '';
@@ -18,4 +21,11 @@ function addCollegesToTable(collegeList) {
 
 document.addEventListener('DOMContentLoaded', () => {
   tableBody.innerHTML = addCollegesToTable(colleges);
+});
+
+searchButton.addEventListener('click', async () => {
+  const stateSearchInput = document.querySelector('#js-state-search-box').value;
+  const citySearchInput = document.querySelector('#js-city-search-box').value;
+  const colleges2 = await fetchCollege(stateSearchInput, citySearchInput);
+  tableBody.innerHTML = addCollegesToTable(colleges2);
 });
